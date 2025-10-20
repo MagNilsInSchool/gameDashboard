c3a33f9e8e02a5742fdd8923533edd8ef54edf28
Regnhundar <regnhundar@gmail.com>
2025-10-20 11:33:29 +0200

    made basic user crud operations and zod schemas.

---
+++ backend/test.js
@@ -0,0 +1,6 @@
+const stringLength = (string) => {
+    return string.length;
+};
+
+const string = "Anna-Katarina";
+console.log(string.length);
--- frontend/src/stores/userStore.tsx
+++ frontend/src/stores/userStore.tsx
@@ -1,11 +1,11 @@
 import { create } from "zustand";
 import type { iUser } from "../interfaces/users";
 
-interface UsersStore {
+interface UserStore {
     user: iUser;
     setUser: (user: iUser | ((prev: iUser) => iUser)) => void;
 }
-const useUsersStore = create<UsersStore>((set) => ({
+const useUserStore = create<UserStore>((set) => ({
     user: { id: 1, firstName: "Bean", lastName: "Beanarius", src: "/assets/icons/user-filled.svg" },
     setUser: (value) =>
         set((state) => ({
@@ -13,4 +13,4 @@ const useUsersStore = create<UsersStore>((set) => ({
         })),
 }));
 
-export default useUsersStore;
+export default useUserStore;
